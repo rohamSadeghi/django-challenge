060f5916f52a4bb000f62ee1a2ad1bae1d360076
Roham Sadeghi <r.sadeghi@hami.blue>
2021-01-18 11:05:44 +0330

    Add max to create manager&Add stadium serializer

--- stadiums/api/serializers.py
+++ stadiums/api/serializers.py
@@ -4,17 +4,24 @@ from django.utils.translation import ugettext_lazy as _
 from rest_framework import serializers
 from rest_framework.generics import get_object_or_404
 
-from stadiums.models import Match, Seat
+from stadiums.models import Match, Seat, Stadium
 from transactions.models import PurchaseMatch
 
 
+class StadiumSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stadium
+        fields = ['id', 'name', 'address', 'phone_number', ]
+
+
 class MatchSerializer(serializers.ModelSerializer):
+    stadium = StadiumSerializer(read_only=True)
     seat_id = serializers.IntegerField(write_only=True)
 
     class Meta:
         model = Match
         fields = ['id', 'title', 'seat_id', 'stadium', 'execute_time_range', ]
-        read_only_fields = ['id', 'title', 'stadium', 'execute_time_range', ]
+        read_only_fields = ['id', 'title', 'execute_time_range', ]
 
     def create(self, validated_data):
         seat_id = validated_data['seat_id']
@@ -29,6 +36,6 @@ class MatchSerializer(serializers.ModelSerializer):
                 match=match,
                 is_paid=True,
                 user=validated_data['user'],
-                price=seat.price - seat.price_discount
+                price=max(seat.price - seat.price_discount, 0)
             )
         return match
